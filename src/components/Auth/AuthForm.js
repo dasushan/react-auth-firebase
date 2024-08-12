8685919c043ea5638e8deef5d50a8206aea5c263
dasushan <53913896+dasushan@users.noreply.github.com>
2024-08-12 09:20:35 +0530

    adding login feature and error modal

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -22,6 +22,33 @@ const AuthForm = () => {
         // optional: Add validation
         setIsLoading(true);
         if(isLogin) {
+            fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBKniczVLNJrXICnBbwj2W29ttGPgAtKCY',{
+                method: 'POST',
+                body: JSON.stringify({
+                    email: enteredEmail,
+                    password: enteredPassword,
+                    returnSecureToken: true
+                }),
+                headers: {
+                    'Content-Type' : 'application/json'
+                }
+            }).then((res) => {
+                setIsLoading(false);
+                if(res.ok){
+                    return res.json().then(data => {
+                        console.log(data.idToken);
+                    }) 
+                } else{
+                    return res.json().then(data => {
+                        // show an error modal
+                        let errorMessage = 'Authentication failed!';
+                        if(data && data.error && data.error.message){
+                            errorMessage = data.error.message;
+                        }
+                        alert(errorMessage)
+                    })
+                }
+            })
 
         } else{
             fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBKniczVLNJrXICnBbwj2W29ttGPgAtKCY', {
